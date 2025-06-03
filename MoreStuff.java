8a8bf4c1a35d8593ce3b0b64346c3c8cdb7ddd09
vswnz <61894102+vswnz@users.noreply.github.com>
2025-06-04 11:44:42 +1200

    Add files via upload
    First upload

---
+++ MoreStuff.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class MoreStuff here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class MoreStuff
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    
+    public MoreStuff()
+    {
+     // TBC
+    }
+
+    
+}
---
+++ Stuff.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class Stuff here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Stuff
+{
+ 
+    public Stuff()
+    {
+       System.out.println("Testing");
+    }
+
+    
+}
